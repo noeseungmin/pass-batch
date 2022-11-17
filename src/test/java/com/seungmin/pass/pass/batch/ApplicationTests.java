2a64a8323e3bf1c8d9543c3f693cde4c293133e5
seungmin <nowinmin96@gmail.com>
2022-11-17 18:35:09 +0900

    새로운 프로젝트 GIT 연결

---
+++ src/test/java/com/seungmin/pass/pass/batch/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seungmin.pass.pass.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
